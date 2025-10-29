f725c1b2d21b18d6def0466664c64f82a2155d49
anilgyawali <anilgyawali38@gmail.com>
2025-10-29 13:04:11 +0545

    this is last change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
    build: {
-    outDir: "dist",
+    outDir: "public",
     chunkSizeWarningLimit:2000 // change from "dist" to "public"
   }
 })
